ad5cc705b858f621ba6267f08cf6cf86e65d3f7d
PrajithArjunanS <ps0663@srmist.edu.in>
2026-03-05 08:46:59 +0530

    UC1: Implement application entry and welcome message

---
+++ UseCase1HotelBookingApp.java
@@ -0,0 +1,13 @@
+/**
+ * Use Case 1: Application Entry & Welcome Message
+ * 
+ * Entry point of the Book My Stay application.
+ * Prints the application name and version.
+ * @version 1.0
+ */
+public class UseCase1HotelBookingApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Book My Stay");
+        System.out.println("Hotel Booking System v1.0");
+    }
+}
