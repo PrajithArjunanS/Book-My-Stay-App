8994a3dae6dd42f4317fe533a60bf90d721ffba0
PrajithArjunanS <ps0663@srmist.edu.in>
2026-03-05 09:11:02 +0530

    UC2: Implement room hierarchy and static availability

---
+++ usecase2/UseCase2RoomInitialization.java
@@ -0,0 +1,28 @@
+package usecase2;
+public class UseCase2RoomInitialization {
+
+    public static void main(String[] args) {
+
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
+        System.out.println("Hotel Room Initialization\n");
+
+        System.out.println("Single Room:");
+        single.displayRoomDetails();
+        System.out.println("Available: " + singleAvailable + "\n");
+
+        System.out.println("Double Room:");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleAvailable + "\n");
+
+        System.out.println("Suite Room:");
+        suite.displayRoomDetails();
+        System.out.println("Available: " + suiteAvailable);
+    }
+}
